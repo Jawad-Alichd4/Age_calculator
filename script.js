f04d256c64e066ec7f20fe4196f911ab3fabe864
Jawad-Alichd4 <jawadalichd4@gmail.com>
2024-09-29 10:32:20 -0700

    make some changes

--- script.js
+++ script.js
@@ -10,14 +10,45 @@ function calculateAge() {
     var year = currentDate.getFullYear();
     var month = currentDate.getMonth();
     var day = currentDate.getDate();
-    var dateInput = document.getElementById("DateInput").nodeValue;
-   var myageInMilliseconds = currentDate - dateInput;
+    var dateInput = document.getElementById("DateInput");
+    var dateInputvalue = dateInput.value;
+    var dateInputInOrder  = new Date(dateInputvalue);
 
-   var myageIndays = myageInMilliseconds/(1000 * 60 * 60 * 24);
-   var myageInMonths = myageInMilliseconds/(1000 * 60 * 60);
-   var myageInYears = myageInMilliseconds/(1000 * 60);
 
-    console.log(myageInYears);
+    // console.log(dateInputInOrder);
+    // calculating days months and years
+
+   var myageInMilliseconds = currentDate - dateInputInOrder;
+
+   var myageIndays = Math.floor(myageInMilliseconds / (1000 * 60 * 60 * 24));
+   var myageInMonths = myageIndays / 30;
+   var myageInYears = myageInMonths / 12;
+//    console.log(currentDate);
+//    console.log(myageInYears)
+
+    // console.log(myageInMonths);
+    // console.log(myageIndays);
+    // console.log(myageInMilliseconds);
+
+    
+   var agelist = document.getAnimations("listItems")
+    var li1 = document.createElement('li');
+    li1.value = myageInYears;
+    agelist.appendChild('li');
+
+    var li2 = document.createElement('li');
+    li2.value = myageInMonths;
+    agelist.appendChild('li')
+
+    var li3 = document.createElement('li');
+    li3.value = myageIndays;
+    agelist.appendChild('li');
+
+    
+
+
+
+
 
 
 }
